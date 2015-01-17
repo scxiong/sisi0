3474ee9340630a75fce04207de707c329544f8e6
sc xiong <sc.xiong@qq.com>
2015-01-17 19:45:16 +0100

    structrally working

--- web.js
+++ web.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var fs = require('fs');
 var htmlfile = "index.html";
+var path = require('path')
 
 var app = express.createServer(express.logger());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(request, response) {
   var html = fs.readFileSync('index.html').toString();
+  response.send(html);
+});
+
+app.get('/en', function(request, response) {
+  var html = fs.readFileSync('english.html').toString();
+  response.send(html);
+});
+
+app.get('/cn', function(request, response) {
+  var html = fs.readFileSync('chinese.html').toString();
   response.send(html);
 });
 
